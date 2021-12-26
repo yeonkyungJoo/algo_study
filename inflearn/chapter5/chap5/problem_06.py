5a3a8a7b25546e54329c93b73fd4632894c7fa40
SungsikYang <71358285+sungsikyang92@users.noreply.github.com>
2021-12-26 21:56:41 +0900

    Delete inflearn/chapter5/chap5 directory

--- inflearn/chapter5/chap5/problem_06.py
+++
@@ -1,9 +0,0 @@
-# 응급실
-import sys
-from collections import deque
-sys.stdin = open('input5.txt', 'r')
-N, M = map(int, input().split())
-risky = list(map(int, input().split()))
-r = deque(risky)
-cnt = 0
-

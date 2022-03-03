8d1b904185cf88cbe0d40aa9f4157a41f84c0c72
yeonkyungJoo <younkyong94@gmail.com>
2022-03-04 07:52:15 +0900

    2022.03.04

--- hard-core/baekjoon/group/21922.py
+++ hard-core/baekjoon/group/21922.py
@@ -1,1 +1,50 @@
 N, M = map(int, input().split())
+place = []
+for _ in range(N):
+    place.append(list(map(int, input().split())))
+# print(place)
+
+arr = [[0 for _ in range(N)] for _ in range(M)]
+_x, _y = 5, 4
+# 서
+x, y = _x, _y
+while True:
+    if x < 0 or y < 0 or x >= N or y >= M:
+        break
+    arr[x][y] = 1
+    if place[x][y] == 1:
+        break
+    x -= 1
+
+# 동
+x, y = _x, _y
+while True:
+    if x < 0 or y < 0 or x >= N or y >= M:
+        break
+    arr[x][y] = 1
+    if place[x][y] == 1:
+        break
+    x += 1
+
+# 남
+x, y = _x, _y
+while True:
+    if x < 0 or y < 0 or x >= N or y >= M:
+        break
+    arr[x][y] = 1
+    if place[x][y] == 2:
+        break
+    y += 1
+
+# 북
+x, y = _x, _y
+while True:
+    if x < 0 or y < 0 or x >= N or y >= M:
+        break
+    arr[x][y] = 1
+    if place[x][y] == 2:
+        break
+    y -= 1
+
+print(arr)
+

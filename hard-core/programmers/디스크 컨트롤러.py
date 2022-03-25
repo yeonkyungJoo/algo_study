a326bb47cfb869510e064022cd15d0b18669ffe8
yeonkyung <younkyong94@gmail.com>
2022-03-25 13:43:53 +0900

    2022.03.25

--- hard-core/programmers/디스크 컨트롤러.py
+++ hard-core/programmers/디스크 컨트롤러.py
@@ -1,7 +1,28 @@
+import heapq
+
 def solution(jobs):
     answer = 0
-    return answer
+
+    jobs.sort()
+    i = 0
+    start, now = -1, 0
+    queue = []
+    while i < len(jobs):
+
+        for job in jobs:
+            if start < job[0] <= now:
+                heapq.heappush(queue, (job[1], job[0]))
+        if len(queue) > 0:
+            _job = heapq.heappop(queue)
+            start = now
+            now += _job[0]
+            answer += (now - _job[1])
+            i += 1
+        else:
+            now += 1
+
+    return answer // len(jobs)
 
 if __name__ == "__main__":
-    jobs = [[0, 3], [1, 9], [2, 6]]
+    jobs = [[0, 3], [4, 5], [7, 3]]
     print(solution(jobs))
--- hard-core/programmers/이중우선순위큐.py
+++ hard-core/programmers/이중우선순위큐.py
@@ -4,8 +4,6 @@ def solution(operations):
     queue = []
     _queue = []
 
-    heapq.heapify(queue)
-    heapq.heapify(_queue)
     size = 0
     for op in operations:
         n = int(op.split()[-1])
@@ -17,13 +15,59 @@ def solution(operations):
             if size != 0:
                 size -= 1
                 if n == -1:
-                    heapq.heappop(queue)
+                    v1 = heapq.heappop(queue)
+                    _queue.remove(v1 * (-1))
                 elif n == 1:
-                    heapq.heappop(_queue)
-        print(queue, _queue)
+                    v2 = heapq.heappop(_queue)
+                    queue.remove(v2 * (-1))
     if size == 0:
         return [0, 0]
     return [heapq.heappop(_queue) * (-1), heapq.heappop(queue)]
+
+'''
+import heapq
+
+def solution(operations):
+    min_heap = list()
+    max_heap = list()
+
+    insert = 0
+    delete = 0
+    for operation in operations:
+        op, n = operation.split()
+        n = int(n)
+        if op == 'I':
+            insert += 1
+            heapq.heappush(min_heap, n)
+            heapq.heappush(max_heap, n * (-1))
+        else:
+            delete += 1
+            if n == -1:
+                if len(min_heap) != 0:
+                    heapq.heappop(min_heap)
+            else:
+                if len(max_heap) != 0:
+                    heapq.heappop(max_heap)
+
+    if delete >= insert:
+        return [0, 0]
+    else:
+        m = heapq.heappop(min_heap)
+        while True:
+            if m * (-1) not in max_heap:
+                m = heapq.heappop(min_heap)
+            else:
+                break
+
+        n = heapq.heappop(max_heap)
+        while True:
+            if n * (-1) not in min_heap:
+                n = heapq.heappop(max_heap)
+            else:
+                break
+
+        return [n * (-1), m]
+'''
 
 if __name__ == "__main__":
     operations = ["I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"]
